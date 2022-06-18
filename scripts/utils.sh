ca3934e2c81966d4514311b0dc09871b327bdf8e
harris <ma402847591@gmail.com>
2022-06-18 16:34:14 +0800

    feat(git): 修改git地址

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -331,7 +331,7 @@ EOF
 }
 
 function get_latest_version() {
-  curl -s 'https://api.github.com/repos/hummerrisk/hummerrisk/releases/latest' |
+  curl -s 'https://api.github.com/repos/HummerRisk/HummerRisk/releases/latest' |
     grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' |
     sed 's/\"//g;s/,//g;s/ //g'
 }
