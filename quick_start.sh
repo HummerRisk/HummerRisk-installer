24b129882794f63b8157754f95c9f2ac49e9e62a
Alvin <alvin@hummercloud.com>
2023-09-05 10:32:38 +0800

    chore(install): 仓库地址替换为 chaitin

--- quick_start.sh
+++ quick_start.sh
@@ -3,14 +3,14 @@
 
 set -e
 export CURRENT_DIR=$(cd "$(dirname "$0")";pwd)
-export VERSION=$(curl -s https://api.github.com/repos/HummerRisk/HummerRisk/releases/latest | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
+export VERSION=$(curl -s https://api.github.com/repos/chaitin/HummerRisk/releases/latest | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
 
 hummerrisk_online_file_name="hummerrisk-installer-${VERSION}.tar.gz"
 
 function get_installer() {
   echo "Download install script to hummerrisk-installer-${VERSION} (开始下载安装脚本到 hummerrisk-installer-${VERSION})"
   if [ ! -d "hummerrisk-installer-${VERSION}" ]; then
-    curl -LOk -m 60 -o "${hummerrisk_online_file_name}" https://github.com/hummerrisk/hummerrisk/releases/download/"${VERSION}"/"${hummerrisk_online_file_name}" || {
+    curl -LOk -m 60 -o "${hummerrisk_online_file_name}" https://github.com/chaitin/hummerrisk/releases/download/"${VERSION}"/"${hummerrisk_online_file_name}" || {
     rm -rf "${hummerrisk_online_file_name}"
     echo -e "[\033[31m ERROR \033[0m] Failed to download hummerrisk-installer-${VERSION} (下载 hummerrisk-installer-${VERSION} 失败, 请检查网络是否正常或尝试重新执行脚本)"
     exit 1
